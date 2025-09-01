cc169114c77d3475874947b46d38b4ece2b1a46d
Tayane <emanuelytayane604@gmail.com>
2025-09-01 07:42:24 -0400

    continue

--- contact/admin.py
+++ contact/admin.py
@@ -6,4 +6,10 @@ from contact import models
 class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'first_name', 'last_name', 'phone',
     ordering = '-id',
-search_fields = 'id', 'first_name', 'last_name',
+    search_fields = 'id', 'first_name', 'last_name',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
+ 
