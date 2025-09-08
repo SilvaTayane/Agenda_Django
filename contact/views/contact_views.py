8ae0eb288d74d357f4f24f8e51fb744d92f67aea
Tayane <emanuelytayane604@gmail.com>
2025-09-08 10:08:59 -0400

    continue_frontend

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,16 +1,36 @@
+from django.http import Http404
 from django.shortcuts import render
 from contact.models import Contact
 
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True)[:10]
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
     }
 
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+def contact(request, contact_id):
+    single_contact = Contact.objects.filter(pk=contact_id).first()
+
+    if single_contact is None:
+        raise Http404()
+
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+    context = {
+        'contact': single_contact,
+        'site_title': site_title,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context
     )
