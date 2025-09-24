276a29220032dca95ffaf0849ab091763c776228
Tayane <emanuelytayane604@gmail.com>
2025-09-24 19:59:47 -0400

    continue_

---
+++ contact/forms.py
@@ -0,0 +1,41 @@
+from contact.models import Contact
+from django import forms
+from  django.core.exceptions import ValidationError
+from . import models
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = models.Contact
+        fields = (
+            'first_name', 'last_name', 'phone', 'email', 
+            'description', 'category', 'picture'
+        )
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                    'O primeiro nome não pode igual ao segundo nome',
+                    code='invalid'
+                )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+        return super().clean()
+
+    def clean_first_name(self):
+        first_name =  self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Erro',
+                    code='invalid'
+                )
+            )
+
+        return first_name
---
+++ contact/views/contact_forms.py
@@ -0,0 +1,28 @@
+from contact.forms import ContactForm
+from django.shortcuts import render
+
+
+# Create your views here.
+
+
+
+def create(request):
+    if request.method == 'POST':
+        context = {
+        'form': ContactForm(request.POST)
+        }
+
+        return render(
+        request,
+        'contact/create.html',
+        context
+        )
+
+    context = {
+        'form': ContactForm()
+    }
+    return render(
+        request,
+        'contact/create.html',
+        context
+    )
